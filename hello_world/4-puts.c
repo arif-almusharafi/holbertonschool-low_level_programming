3a7a81de8a6c27bfa97460c80f75cc91a4fe94d3
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-08 14:10:10 +0300

    Fix formatting in 4-puts.c and update 4-puts.exe binary

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,9 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle"); // Line of code that prints a string to the console
-    return (0); // Return statement indicating successful execution
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
