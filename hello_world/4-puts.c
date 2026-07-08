31134397b9f09e084df0187db9c49a7d8676fba5
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-08 14:04:16 +0300

    Fix LF

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,8 +4,9 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+    puts("\"Programming is like building a multilingual puzzle"); // Line of code that prints a string to the console
+    return (0); // Return statement indicating successful execution
 }
