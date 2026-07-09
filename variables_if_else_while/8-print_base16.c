2ea991a9c0d6b7b3b0795fa690ecffe99843105d
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-09 14:25:40 +0300

    Refactor code structure for improved readability and maintainability

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+n = 0;
+while (n < 16)
+{
+if (n < 10)
+putchar('0' + n);
+else
+putchar('a' + n - 10);
+n++;
+}
+putchar('\n');
+return (0);
+
+}
