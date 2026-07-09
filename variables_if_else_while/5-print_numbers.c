eeb0ffa1804097c71d56842164db757aff3c0539
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-09 14:14:49 +0300

    Refactor 5-print_numbers.c for improved readability and maintainability

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n = 0;
-while (n < 10){
-    printf("%d", n);
-    n++;
-}
-putchar('\n');
-return (0);
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+
+	printf("\n");
+
+	return (0);
 }
