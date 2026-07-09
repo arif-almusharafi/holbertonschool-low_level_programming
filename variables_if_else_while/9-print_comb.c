834c618aac8bd836b4596a8bfbf5e8b2c814e06e
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-09 14:30:28 +0300

    Refactor 9-print_comb.c for improved readability and maintainability

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints all single digit numbers separated by comma.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
-    for (n = 0; n < 10; n++)
-    {
-        putchar('0' + n);
-        if (n != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return (0);
+	int n;
 
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
