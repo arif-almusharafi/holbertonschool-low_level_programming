4c6b11340761f908345f4dd7e6545bc3c55ad528
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-09 14:20:43 +0300

    Refactor 7-print_tebahpla.c for improved readability and maintainability

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints the alphabet in reverse.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char letter;
-    for (letter = 'z'; letter >= 'a'; letter--)
-    {
-        putchar(letter);
-    }
-    putchar('\n');
-    return (0);
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
