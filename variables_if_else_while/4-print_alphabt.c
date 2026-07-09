dcbf58e919ea5be221fb12290435c966043aa60c
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-09 14:04:47 +0300

    Update 4-print_alphabt.c to clarify function description and improve code formatting

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints the alphabet except q and e.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char letter;
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        if (letter != 'e' && letter != 'q')
-        putchar(letter);
+	char letter;
 
-    }
-    putchar('\n');
-    return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
