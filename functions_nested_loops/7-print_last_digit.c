c3b77c876d1bc2b938da028011417b2c2d613ab5
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-10 18:30:14 +0300

    Fix print_last_digit function implementation and update its prototype in main.h

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to extract the last digit from
  *
- * Return: void
+ * Return: the last digit of n
  */
-int print_last_digit(int last_digit)
+int print_last_digit(int n)
 {
-    return (last_digit % 10);
+    if (n < 0)
+        n = -n;
+    _putchar(n % 10 + '0');
 }
