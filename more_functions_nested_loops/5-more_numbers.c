84bf96495dce46d4c2dc77ef171b92fadfc74562
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-11 02:15:46 +0300

    Fix more_numbers function to correctly print numbers from 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,16 @@
  */
 void more_numbers(void)
 {
+    int i;
 	int num;
-    for (int i = 0; i < 10; i++)
+    for (i = 0; i < 10; i++)
     {
 
 	    for (num = 0; num <= 14; num++)
         {
-            _putchar((num % 10) + '0');
             _putchar((num / 10) + '0');
+            _putchar((num % 10) + '0');
+            
         }
         _putchar('\n');
     }
