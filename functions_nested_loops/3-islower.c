ba29d2ab36fb9775c6f6814f9a0a2f90cde75fb2
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-10 16:52:43 +0300

    Add _islower function to check for lowercase letters

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+void _islower(int c)
+{
+    if (c >= 'a' && c <= 'z')
+    {
+        _putchar(1);
+    }
+    else
+    {
+        _putchar(0);
+    }
+}
