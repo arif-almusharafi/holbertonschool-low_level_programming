b87022144e18f8dd71012696eefe1678229013b7
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-11 02:41:37 +0300

    edit Betty coding style in 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,23 +2,24 @@
 
 /**
  * print_diagonal - Prints a diagonal line of the specified length.
- * @n: The length of the diagonal line to print.
+ * @n: The length of the diagonal line.
  */
 void print_diagonal(int n)
 {
 	int i;
 	int j;
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
-		{
 			_putchar(' ');
-		}
+
 		_putchar('\\');
 		_putchar('\n');
 	}
