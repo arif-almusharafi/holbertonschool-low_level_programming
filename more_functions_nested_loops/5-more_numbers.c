dba57811964ec6fd7ce938c020b792f2253145d2
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-11 02:14:13 +0300

    Fix more_numbers function to correctly print tens place for numbers 10-14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,6 +12,7 @@ void more_numbers(void)
 	    for (num = 0; num <= 14; num++)
         {
             _putchar((num % 10) + '0');
+            _putchar((num / 10) + '0');
         }
         _putchar('\n');
     }
