564a5fd3722bb89e465d63cdc1cc7527fd474aab
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-11 02:40:13 +0300

    Fix print_diagonal function to handle non-positive lengths correctly

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,8 +8,12 @@ void print_diagonal(int n)
 {
 	int i;
 	int j;
-
-	for (i = 0; i <= n; i++)
+    if (n <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
