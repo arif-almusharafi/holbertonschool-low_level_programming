1c6c62ff36735d22769a5298b53c46a2d11c1031
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-11 02:55:50 +0300

    Refactor print_square function documentation for clarity and consistency

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * print_diagonal - Prints a diagonal line of the specified length.
- * @n: The length of the diagonal line to print.
+ * print_square - Prints a square.
+ * @size: The size of the square.
  */
 void print_square(int size)
 {
-    int i, j;
+	int i;
+	int j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
 }
