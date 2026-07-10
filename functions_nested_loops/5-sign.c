af84a0344f85f11af799edccd8a8c31babec227c
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-10 17:19:56 +0300

    Add print_sign function to determine the sign of an integer

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+int prist_sign(int n)
+{
+    if (n > 0)
+    {
+        return (1);
+    }
+    else if (n < 0)
+    {
+        return (-1);
+    }
+    else
+    {
+        return (0);
+    }
+}
