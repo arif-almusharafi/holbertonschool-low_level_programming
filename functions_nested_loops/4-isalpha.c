2459d9bb71b0c10675253e7bf7e6890afcabc262
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-10 17:15:35 +0300

    Refactor _isalpha function to simplify character checks

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -7,16 +7,11 @@
  */
 int _isalpha(int c)
 {
-    if (c >= "A" &&  c <= "Z")
-    {
-        return (1);
-    }
-    else if (c >= "a" && c <= "z")
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
     {
         return (1);
     }
     else
     {
         return (0);
-    }
     }
