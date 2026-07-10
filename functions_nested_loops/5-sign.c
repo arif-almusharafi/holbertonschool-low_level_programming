3fd115e6f59a3ecf712df820835a4cdada27b553
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-10 17:24:17 +0300

    Fix print_sign function to output sign characters after return statements

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,13 +10,19 @@ int print_sign(int n)
     if (n > 0)
     {
         return (1);
+        _putchar('+');
+        _putchar('\n');
     }
     else if (n < 0)
     {
         return (-1);
+        _putchar('-');
+        _putchar('\n');
     }
     else
     {
         return (0);
+        _putchar('0');
+        _putchar('\n');
     }
 }
