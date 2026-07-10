b4c1225c8681efd929b65964a5ceab4e023dff7f
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-10 17:38:12 +0300

    Add _abs function to compute absolute value and update header file

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+int _abs(int)
+{
+    int n;
+
+    if (n < 0)
+        return (-n);
+    else
+        return (n);
+}
