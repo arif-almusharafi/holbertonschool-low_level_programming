6f54339daee6a32c11060974bc760b5d1271f03c
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-10 21:23:12 +0300

    Refactor _isupper function for consistency and clarity

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * _isupper - Checks for an uppercase character.
+ * @c: Character to check.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
 }
