2d56a789f0414ac5131ca8a498d3e59666054f24
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-10 16:49:00 +0300

    Refactor print_alphabet_x10 function for improved readability

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase
+ * print_alphabet_x10 - prints the alphabet 10 times
  *
  * Return: void
  */
-
 void print_alphabet_x10(void)
 {
+	char letter;
+	int i;
 
-    char letter;
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
 }
