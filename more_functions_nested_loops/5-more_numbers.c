f20818440af25fe45a0acd27eeccb9c805890e79
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-11 02:27:46 +0300

    Refactor more_numbers function to improve code readability and maintainability

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints the numbers from 0 to 14, excluding 2 and 4.
+ * more_numbers - Prints the numbers from 0 to 14 ten times.
  */
 void more_numbers(void)
 {
-    int i;
+	int i;
 	int num;
-    for (i = 0; i < 10; i++)
-    {
 
-	    for (num = 0; num <= 14; num++)
-        {
-            if (num >= 10)
-            {
-            _putchar((num / 10) + '0');
-            }
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+				_putchar((num / 10) + '0');
 
-            _putchar((num % 10) + '0');
-            
-        }
-        _putchar('\n');
-    }
-    
+			_putchar((num % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
 }
