73d6aa674e6210dcd89cd636ef1a8bc871eb757f
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-14 12:51:09 +0300

    Add timing measurements for dataset processing in instrumentation_lab.c

--- benchmarking/instrumentation_lab.c
+++ benchmarking/instrumentation_lab.c
@@ -52,23 +52,46 @@ static unsigned long reduce_checksum(void)
 
 int main(void)
 {
+    clock_t total_start, total_end;
+    clock_t build_start, build_end;
+    clock_t process_start, process_end;
+    clock_t reduce_start, reduce_end;
+
+    double total_time;
+    double build_time;
+    double process_time;
+    double reduce_time;
+
     unsigned long checksum;
 
-    /* Students must add clock-based timing and print required lines. */
+    total_start = clock();
 
+    build_start = clock();
     build_dataset();
+    build_end = clock();
+
+    process_start = clock();
     process_dataset();
+    process_end = clock();
+
+    reduce_start = clock();
     checksum = reduce_checksum();
+    reduce_end = clock();
+
+    total_end = clock();
+
+    build_time = (double)(build_end - build_start) / CLOCKS_PER_SEC;
+    process_time = (double)(process_end - process_start) / CLOCKS_PER_SEC;
+    reduce_time = (double)(reduce_end - reduce_start) / CLOCKS_PER_SEC;
+    total_time = (double)(total_end - total_start) / CLOCKS_PER_SEC;
 
     if (checksum == 0ul)
         printf("impossible\n");
 
-    /* Required output (exact format, no extra lines):
-     * TOTAL seconds: <float>
-     * BUILD_DATA seconds: <float>
-     * PROCESS seconds: <float>
-     * REDUCE seconds: <float>
-     */
+    printf("TOTAL seconds: %.6f\n", total_time);
+    printf("BUILD_DATA seconds: %.6f\n", build_time);
+    printf("PROCESS seconds: %.6f\n", process_time);
+    printf("REDUCE seconds: %.6f\n", reduce_time);
 
-    return 0;
+    return (0);
 }
