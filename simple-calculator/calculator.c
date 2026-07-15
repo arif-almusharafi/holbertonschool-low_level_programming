25e4f709f7db7daff0c1b600c7415118894892b4
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-15 20:11:56 +0300

    Refactor calculator code to uncomment user input prompt and clean up operator handling logic

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -2,44 +2,14 @@
 
 int main(void)
 {
-    printf("Simple Calculator\n");
-
     int operator;
     int a,b;
+
+    printf("Simple Calculator\n");
+
+
     printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
     scanf("%d", &operator);
-    // if (operator == 1)
-    // {
-    //     // printf("Enter two numbers:");
-    //     // scanf("%d %d", &a, &b);
-    //     // printf("Result: %d\n", a + b);
-
-    // }
-    // else if (operator == 2)
-    // {
-    //     // printf("Enter two numbers:");
-    //     // scanf("%d %d", &a, &b);
-    //     // printf("Result: %d\n", a - b);
-    // }
-    // else if (operator == 3)
-    // {
-    //     // printf("Enter two numbers:");
-    //     // scanf("%d %d", &a, &b);
-    //     // printf("Result: %d\n", a * b);
-    // }
-    // else if (operator == 4)
-    // {
-    //     // printf("Enter two numbers:");
-    //     // scanf("%d %d", &a, &b);
-    //     // if (b == 0)
-    //     // {
-    //     //     printf("Error: Division by zero\n");
-    //     // }
-    //     // else
-    //     // {
-    //     //     printf("Result: %d\n", a / b);
-    //     // }
-    // }
     if (operator == 0)
     {
         printf("Bye!\n");
