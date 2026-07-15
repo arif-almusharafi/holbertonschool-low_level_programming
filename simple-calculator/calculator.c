56f4bfa0fc8a54e7154174b87d81a724c05d13c7
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-15 12:35:04 +0300

    Refactor calculator code to comment out operator handling logic

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -8,39 +8,39 @@ int main(void)
     // int a,b;
     printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
     scanf("%d", &operator);
-    if (operator == 1)
-    {
-        // printf("Enter two numbers:");
-        // scanf("%d %d", &a, &b);
-        // printf("Result: %d\n", a + b);
+    // if (operator == 1)
+    // {
+    //     // printf("Enter two numbers:");
+    //     // scanf("%d %d", &a, &b);
+    //     // printf("Result: %d\n", a + b);
 
-    }
-    else if (operator == 2)
-    {
-        // printf("Enter two numbers:");
-        // scanf("%d %d", &a, &b);
-        // printf("Result: %d\n", a - b);
-    }
-    else if (operator == 3)
-    {
-        // printf("Enter two numbers:");
-        // scanf("%d %d", &a, &b);
-        // printf("Result: %d\n", a * b);
-    }
-    else if (operator == 4)
-    {
-        // printf("Enter two numbers:");
-        // scanf("%d %d", &a, &b);
-        // if (b == 0)
-        // {
-        //     printf("Error: Division by zero\n");
-        // }
-        // else
-        // {
-        //     printf("Result: %d\n", a / b);
-        // }
-    }
-    else if (operator == 0)
+    // }
+    // else if (operator == 2)
+    // {
+    //     // printf("Enter two numbers:");
+    //     // scanf("%d %d", &a, &b);
+    //     // printf("Result: %d\n", a - b);
+    // }
+    // else if (operator == 3)
+    // {
+    //     // printf("Enter two numbers:");
+    //     // scanf("%d %d", &a, &b);
+    //     // printf("Result: %d\n", a * b);
+    // }
+    // else if (operator == 4)
+    // {
+    //     // printf("Enter two numbers:");
+    //     // scanf("%d %d", &a, &b);
+    //     // if (b == 0)
+    //     // {
+    //     //     printf("Error: Division by zero\n");
+    //     // }
+    //     // else
+    //     // {
+    //     //     printf("Result: %d\n", a / b);
+    //     // }
+    // }
+    if (operator == 0)
     {
         printf("Bye!\n");
     }
