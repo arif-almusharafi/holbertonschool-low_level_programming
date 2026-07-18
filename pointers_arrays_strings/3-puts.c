3941670177ea6521a9b7522cb5b5e1f466201667
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-18 20:28:39 +0300

    Add _puts function to print a string followed by a new line

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line.
+ * @str: Pointer to the string to be printed.
+ *
+ * Return: Nothing.
+ */
+void _puts(char *str)
+{
+    while (*str != '\0')
+    {
+        _putchar(*str);
+        str++;
+    }
+
+    _putchar('\n');
+}
