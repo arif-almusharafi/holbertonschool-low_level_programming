5c1afef87e809c029085b4808df95cc7f8d1dd1b
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-22 14:58:43 +0300

    Fix create_array function to correct size check and update variable type for loop index

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,10 +5,10 @@
 char *create_array(unsigned int size, char c)
 {
     char *array;
-    int i;
+    unsigned int i;
 
 
-    if (size == 0)
+    if (size == NULL)
     {
         return NULL;
     }
@@ -25,4 +25,5 @@ char *create_array(unsigned int size, char c)
         array[i] = c;
     }
 
+    return create_array;
 }
