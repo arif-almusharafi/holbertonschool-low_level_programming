1fc7e3ee9c07e62759556d4e5102a3bfa0670aa6
arif-almusharafi <arif.m.almusharafi@gmail.com>
2026-07-25 20:19:41 +0300

    Refactor _strdup function for improved readability and consistency

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,31 +1,38 @@
-#include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * _strdup - returns a pointer to a newly allocated space
+ * containing a copy of the string given as a parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the duplicated string, or NULL on failure
+ */
 char *_strdup(char *str)
 {
-    char *ptr = str;
-    unsigned int len = 0;
-    char *copy;
-    unsigned int i;
+	char *ptr;
+	char *copy;
+	unsigned int len;
+	unsigned int i;
 
-    if (str == NULL)
-        return NULL;
+	if (str == NULL)
+		return (NULL);
 
-    while (*ptr != '\0')
-    {
-        len++;
-        ptr++;
-    }
+	ptr = str;
+	len = 0;
 
-    copy = malloc(len + 1);
+	while (*ptr != '\0')
+	{
+		len++;
+		ptr++;
+	}
 
-    if (copy == NULL)
-        return NULL;
+	copy = malloc(len + 1);
 
-    for (i = 0; i <= len; i++)
-    {
-        copy[i] = str[i];
-    }
-    return  copy;
+	if (copy == NULL)
+		return (NULL);
 
+	for (i = 0; i <= len; i++)
+		copy[i] = str[i];
+
+	return (copy);
 }
