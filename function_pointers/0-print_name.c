7724685a5f8735e7b26a6ee744fbc4d086542e3e
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-29 13:03:49 +0300

    Add null checks in print_name function to prevent dereferencing null pointers

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,16 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - prints a name using a function pointer
+ * @name: the name to print
+ * @f: pointer to a function that prints a name
+ *
+ * Return: Nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+    if (name != NULL && f != NULL)
+    {
+        f(name);
+    }
 }
