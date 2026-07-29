4a1ff960e9eb0f62ed253fef349d7bd9cb1fa509
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-29 13:05:09 +0300

    Fix indentation in print_name function for consistency

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+
 /**
  * print_name - prints a name using a function pointer
  * @name: the name to print
@@ -9,8 +10,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-    {
-        f(name);
-    }
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
