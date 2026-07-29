f79d9b5ebf6ba2b269a785a69046894ce8f991cf
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-29 13:50:04 +0300

    Add int_index function to find index of an element in an array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    int i;
+    if (array != NULL && cmp != NULL)
+    {
+        for (i = 0; i < size; i++)
+        {
+            return cmp(array[i]);
+        }
+
+    }
+    else
+    return -1;
+}
