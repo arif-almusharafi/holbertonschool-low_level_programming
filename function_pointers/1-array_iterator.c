a2d48d537e0f1d2a7c72a18a2adf68d72417bfe8
arif.almusharafi <arif.almusharafi@gmai.com>
2026-07-29 13:27:46 +0300

    Fix array_iterator function to correctly iterate over array elements and call action on each element

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,8 +5,11 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
     int i;
-    for (i = 0; i <= size; i++)
+    if(array != NULL && action != NULL)
     {
-        action(array);
+        for (i = 0; i < size; i++)
+        {
+            action(array[i]);
+        }
     }
 }
