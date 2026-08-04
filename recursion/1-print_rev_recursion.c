290cebdb4d348873128d102dd8782abd3644f00e
arif.almusharafi <arif.almusharafi@gmai.com>
2026-08-04 13:19:03 +0300

    Fix recursion calls in _print_rev_recursion function for correct string reversal

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,14 +7,14 @@ void _print_rev_recursion(char *s)
 
     if (s[len] != '\0')
     {
-        len + 1;
+        _print_rev_recursion(len + 1);
     }
-    len - 1;
+    _print_rev_recursion(len - 1);
 
     if (len >= 0)
     {
         _putchar(s[len]);
-        len - 1;
+        _print_rev_recursion(len - 1);
     }
     _putchar('\n');
 
