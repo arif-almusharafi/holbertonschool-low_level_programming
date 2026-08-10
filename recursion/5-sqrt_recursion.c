be967c21f3e09b40a0a91faaee514098eb35aa40
arif.almusharafi <arif.almusharafi@gmai.com>
2026-08-10 15:02:55 +0300

    Add _sqrt_recursion function to calculate the square root of a number

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+int _sqrt_recursion(int n)
+{
+    int x;
+    if (x * x != n)
+    {
+        return -1;
+    }
+    else if (n == 0)
+    {
+        return 1;
+    }
+
+
+    return (x * x = n);
+}
