8a8883226d8bdad5dd791563455c97910dc68b5c
arif.almusharafi <arif.almusharafi@gmai.com>
2026-08-13 15:17:50 +0300

    Fix struct definition for doubly linked list in lists.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -4,19 +4,19 @@
 #include <stddef.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct dlistint_t - doubly linked list
+ * @n: int - (malloc'ed string)
+ * @prev: points to the previous node
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: doubly linked list node structure
  */
-typedef struct list_s
+typedef struct dlistint_t
 {
     const int n;
-	struct list_s *next;
-    struct list_s *prev;
-} list_t;
+	struct dlistint_t *next;
+    struct dlistint_t *prev;
+} dlistint_t;
 
 int _putchar(char c);
 size_t print_dlistint(const dlistint_t *h);
