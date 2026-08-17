7f8bf1f43965d541bc3c130dbc3318e191b8d9f2
arif.almusharafi <arif.almusharafi@gmai.com>
2026-08-17 22:52:54 +0300

    Fix square root recursion logic and update main function for testing

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,18 +1,59 @@
 #include "main.h"
 
 
-int _sqrt_recursion(int n)
+int helper(int n, int current)
 {
-    int x;
-    if (x * x != n)
+    if (current * current < n)
+    {
+        return(helper(current + 1));
+    }
+    else if (current * current > n)
     {
         return -1;
     }
+    else if (current * current == n)
+    {
+        return current;
+    }
+
+}
+
+
+int _sqrt_recursion(int n)
+{
+    int current = 0;
+    if (current * current != n)
+    {
+        return -1;
+    }
+    else if (current * current == n)
+    {
+        return 0;
+    }
+
     else if (n == 0)
     {
         return 1;
     }
 
+    return (_sqrt_recursion(current));
+}
 
-    return (x * x = n);
+int main(void)
+{
+    int r;
+
+    r = _sqrt_recursion(1);
+    printf("%d\n", r);
+    r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16);
+    printf("%d\n", r);
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(-1);
+    printf("%d\n", r);
+    return (0);
 }
