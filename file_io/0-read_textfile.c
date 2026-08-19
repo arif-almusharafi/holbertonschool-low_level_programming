fe6c6f04454b98a6f8594c91c6d1578c68e8152a
arif.almusharafi <arif.almusharafi@gmai.com>
2026-08-19 13:37:15 +0300

    Add read_textfile function for reading and printing text files

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX standard output
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
+ *
+ * Return: Actual number of letters read and printed, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char buffer[letters];
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	if (bytes_read > 0)
+	{
+		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			close(fd);
+			return (0);
+		}
+	}
+
+	close(fd);
+
+	return (bytes_read);
+}
---
+++ file_io/README.md
@@ -0,0 +1,25 @@
+# File I/O
+
+This project is about learning how to work with files in C using POSIX system calls.
+
+## Description
+
+In this project, I learned how to:
+
+- Open and close files using `open()` and `close()`
+- Read data from files using `read()`
+- Write data using `write()`
+- Work with file descriptors
+- Use POSIX file I/O
+- Handle errors when working with files
+- Understand file permissions and flags
+
+## Requirements
+
+- Ubuntu 20.04 LTS
+- GCC
+- Betty coding style
+- All files are compiled with:
+
+```bash
+gcc -Wall -Werror -Wextra -pedantic *.c -o <output>
