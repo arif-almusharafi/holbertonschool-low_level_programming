8cbc4e5599d87dc9bae7b68709490e63b5a5f147
arif.almusharafi <arif.almusharafi@gmai.com>
2026-08-20 14:43:27 +0300

    Refactor append_text_to_file function for improved readability and error handling

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,24 +13,40 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int string_len;
+	int text_len;
 	int bytes_written;
 
 	/* 1. Check filename */
+	if (filename == NULL)
+		return -1;
 
 	/* 2. Open the existing file for writing at the end */
+	fd = open(filename, O_WRONLY);
 
 	/* 3. Check if open failed */
+	if (fd == NULL)
+	{
+		close(fd);
+		return -1;
+	}
 
 	/* 4. If text_content is NULL */
+	if (text_content == NULL)
+		return 1;
 
 	/* 5. Get length of text_content */
+	text_len = strlen(text_content);
 
 	/* 6. Write text to the file */
-
+	bytes_written = write(fd, text_content, text_len);
 	/* 7. Check if write succeeded */
-
+	if (bytes_written != text_len)
+	{
+		close(fd);
+		return -1;
+	}
 	/* 8. Close the file */
-
+	close(fd);
 	/* 9. Return success */
+	return 1;
 }
